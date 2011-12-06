17aeaaf6d55f3695c084ee3b110fbf9d5492e977
eliztowns <townse2@rpi.edu>
2011-12-05 19:28:47 -0500

    MORE FOGGGG

--- Assets/blue_spawn.cs
+++ Assets/blue_spawn.cs
@@ -5,7 +5,7 @@ public class blue_spawn : MonoBehaviour {
 	
 	public float spawnChance = 0.005f;
 	//public int lane;
-	public Vector3 spawnPlace = new Vector3 (5.32399f, 1f, -0.02f);
+	public Vector3 spawnPlace = new Vector3 (5.32399f, 1f, -0.18f);
 	public bluePaint bpPrefab;
 	public float x = 0;
 
@@ -16,15 +16,15 @@ public class blue_spawn : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		/*
+		
 		if(Random.value < spawnChance && bpPrefab)
 		{
 			x = Random.Range(0,100);
 			
 			if(x%2 == 0)
 			{
-				spawnPlace = new Vector3(5.32399f, 1f, -0.2f);
-				Vector3 pos1 = new Vector3(5.32399f, 1f, -0.2f);
+				spawnPlace = new Vector3(5.32399f, 1f, -0.18f);
+				Vector3 pos1 = new Vector3(5.32399f, 1f, -0.18f);
 			
 				bluePaint bp = (bluePaint)Instantiate(bpPrefab);
 				bp.transform.position = spawnPlace;
@@ -33,8 +33,8 @@ public class blue_spawn : MonoBehaviour {
 			}
 			else if(x%3 == 0)
 			{
-				spawnPlace = new Vector3(5.32399f, 1f, -0.0f);
-				Vector3 pos2 = new Vector3(5.32399f, 1f, -0.0f);
+				spawnPlace = new Vector3(5.32399f, 1f, 0f);
+				Vector3 pos2 = new Vector3(5.32399f, 1f, 0f);
 			
 				bluePaint bp = (bluePaint)Instantiate(bpPrefab);
 				bp.transform.position = spawnPlace;
@@ -43,8 +43,8 @@ public class blue_spawn : MonoBehaviour {
 			}
 			else if(x%5 == 0)
 			{
-				spawnPlace = new Vector3(5.32399f, 1f, 0.2f);
-				Vector3 pos3 = new Vector3(5.32399f, 1f, 0.2f);
+				spawnPlace = new Vector3(5.32399f, 1f, 0.18f);
+				Vector3 pos3 = new Vector3(5.32399f, 1f, 0.18f);
 			
 				bluePaint bp = (bluePaint)Instantiate(bpPrefab);
 				bp.transform.position = spawnPlace;
@@ -52,6 +52,6 @@ public class blue_spawn : MonoBehaviour {
 				bp.transform.parent = transform;
 			}
 		}
-		*/
+		
 	}
 }
