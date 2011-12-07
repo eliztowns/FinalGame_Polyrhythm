f368a813c9b344637ae8bfc95e4f677697f2e621
eliztowns <townse2@rpi.edu>
2011-12-06 23:57:27 -0500

    working on orbiter script

--- Assets/Orbiters.cs
+++ Assets/Orbiters.cs
@@ -11,11 +11,13 @@ public class Orbiters : MonoBehaviour {
     public float yoffset;
 	// Use this for initialization
 	void Start () {
-		time_per_loop = 5;
+		float r1 = Random.value;
+		float r2 = Random.value;
+		time_per_loop = r1*2 + 3;
 		top_radius =1;
 		bottom_radius =1;
 		height = .1F;
-		yoffset=1.3F;
+		yoffset=r2 +.7F;
         start_time = Time.time;
         float time_val = (start_time - Time.time) % time_per_loop;
         float radian_val = (2 * Mathf.PI) * (time_val / time_per_loop);
